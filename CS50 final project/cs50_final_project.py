33d14a829047446eb00493cacf87b673f3dcc0ea
Andre Cabrol <113216259+Cabrolse@users.noreply.github.com>
2026-09-14 21:30:38 +0100

    added final project file to repo

---
+++ CS50 final project/cs50_final_project.py
@@ -0,0 +1,25 @@
+import sys
+
+def main():
+
+    if sys.argv[1] == "listing":
+        listing()
+
+
+
+def listing():
+    print("Please enter the following details of your new listing")
+    
+    #user enters details of items which are then saved in unique variables
+    img = input("Images of item: ")
+    name = input("Name of your Item here: ")
+    price = input("Price: ")
+    
+    
+    
+    
+    
+    
+
+
+main
